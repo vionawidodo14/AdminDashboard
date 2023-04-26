0cc8436ea9ca66b283d7b757d00cf096f5a0672d
vionawidodo14 <viona.14apr@gmail.com>
2023-04-26 14:27:40 +0800

    first

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css'
+import MainDash from './components/MainDash/MainDash';
+import Sidebar from './components/Sidebar/Sidebar';
+import RightSide from './components/RightSide/RightSide'
 
 function App() {
   return (
     <div className="App">
-        Subscribe Zainkeepscode
+      <div className="AppGlass">
+        <Sidebar />
+        <MainDash />
+        <RightSide />
+        <div></div>
+      </div>
     </div>
   );
 }
